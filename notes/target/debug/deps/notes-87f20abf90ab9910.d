7d9ad1129c2c7fcaaaec93049abedbf9d06c3386
Jona Ohme <ohmejona@gmail.com>
2023-05-15 16:17:44 +0200

    added gitignore

---
+++ notes/target/debug/deps/notes-87f20abf90ab9910.d
@@ -0,0 +1,5 @@
+/home/ojo/dev/smaller-rust-projects/notes/target/debug/deps/notes-87f20abf90ab9910: src/main.rs
+
+/home/ojo/dev/smaller-rust-projects/notes/target/debug/deps/notes-87f20abf90ab9910.d: src/main.rs
+
+src/main.rs:
---
+++ notes/target/debug/notes.d
@@ -0,0 +1,1 @@
+/home/ojo/dev/smaller-rust-projects/notes/target/debug/notes: /home/ojo/dev/smaller-rust-projects/notes/src/main.rs
