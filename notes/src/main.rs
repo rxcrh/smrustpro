df0193c1bc44e1df9110539368eef19ac7b69472
Jona Ohme <ohmejona@gmail.com>
2023-05-16 12:02:01 +0200

    finished basic crud functions

--- notes/src/main.rs
+++ notes/src/main.rs
@@ -1,8 +1,9 @@
 use rusqlite::{Connection, Result};
 use std::io;
+use dirs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let conn = Connection::open("notes.db")?;
+    let conn = Connection::open(dirs::home_dir().unwrap().to_str().unwrap().to_owned() + "/notes.db")?;
     conn.execute(
         "create table if not exists notes (
             id integer primary key,
@@ -15,10 +16,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     while running == true {
         let mut buffer = String::new();
         io::stdin().read_line(&mut buffer)?;
+
         let trimmed_body = buffer.trim();
-        if trimmed_body == "" {
+        let cmd_split = trimmed_body.split_once(" ");
+
+        let mut cmd = trimmed_body;
+        let mut msg = " ";
+
+        if cmd_split != None {
+            cmd = cmd_split.unwrap().0;
+            msg = cmd_split.unwrap().1;
+        }
+
+        if cmd == "" {
             running = false;
-        } else if trimmed_body == "/list" {
+        } else if cmd == "/del" {
+            let id = msg;
+            conn.execute("DELETE FROM notes WHERE id = (?1)", [id])?;
+        } else if cmd == "/edit" {
+            let msg_split = msg.split_once(" ").unwrap();
+            let id = msg_split.0;
+            let body = msg_split.1;
+            conn.execute("UPDATE notes SET body = (?1) WHERE id = (?2)", [body, id])?;
+        } else if cmd == "/list" {
             let mut stmt = conn.prepare("SELECT id, body FROM notes")?;
             let mut rows = stmt.query(rusqlite::params![])?;
             while let Some(row) = rows.next()? {
