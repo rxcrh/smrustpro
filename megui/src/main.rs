1b998c651d5bafdd5c0db13f6f218f024187985a
Jona Ohme <ohmejona@gmail.com>
2023-05-24 19:09:36 +0200

    changed to Spacebar and used a responsive layout

--- megui/src/main.rs
+++ megui/src/main.rs
@@ -4,7 +4,7 @@ use egui::*;
 fn main() -> Result<(), eframe::Error> {
     let options = eframe::NativeOptions::default();
     eframe::run_native(
-        "Keyboard events",
+        "Spacetester",
         options,
         Box::new(|_cc| Box::<Content>::default()),
     )
@@ -18,25 +18,30 @@ struct Content {
 impl eframe::App for Content {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Press/Hold/Release example. Press A to test.");
-            if ui.button("Clear").clicked() {
-                self.text.clear();
-            }
+            ui.horizontal(|ui| {
+                ui.vertical_centered(|ui| {
+                    ui.heading("Press/Hold/Release. Press your Spacebar to test.");
+                });
+            });
             ScrollArea::vertical()
                 .auto_shrink([false; 2])
-                .stick_to_bottom(true)
+                .max_height(ui.available_height() - 30.)
                 .show(ui, |ui| {
                     ui.label(&self.text);
                 });
-
-            if ctx.input(|i| i.key_pressed(Key::A)) {
+            ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
+                if ui.button("Clear").clicked() {
+                    self.text.clear();
+                }
+            });
+            if ctx.input(|i| i.key_pressed(Key::Space)) {
                 self.text.push_str("\nPressed");
             }
-            if ctx.input(|i| i.key_down(Key::A)) {
+            if ctx.input(|i| i.key_down(Key::Space)) {
                 self.text.push_str("\nHeld");
                 ui.ctx().request_repaint(); // make sure we note the holding.
             }
-            if ctx.input(|i| i.key_released(Key::A)) {
+            if ctx.input(|i| i.key_released(Key::Space)) {
                 self.text.push_str("\nReleased");
             }
         });
