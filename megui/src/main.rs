40128bbb5445a7a9f0d79840e4109f94f8157dcf
Jona Ohme <ohmejona@gmail.com>
2023-05-27 21:10:45 +0200

    the beginning of rustier

--- megui/src/main.rs
+++ megui/src/main.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+
+use image;
 use eframe::egui;
 use egui::*;
 
@@ -12,38 +15,53 @@ fn main() -> Result<(), eframe::Error> {
 
 #[derive(Default)]
 struct Content {
-    text: String,
+    num_tiers: u32,
+    tiers: Vec<String>,
 }
 
 impl eframe::App for Content {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
+            let tier_size: f32 = 0.05 * ui.available_width();
+
+            let texture = ui.ctx().load_texture(
+                "texture",
+                load_image_from_path(Path::new("src/assets/nand_placeholder.png")).unwrap(),
+                Default::default(),
+            );
+
+            ui.add(egui::Image::new(&texture, texture.size_vec2()));
+
             ui.horizontal(|ui| {
-                ui.vertical_centered(|ui| {
-                    ui.heading("Press/Hold/Release. Press your Spacebar to test.");
-                });
-            });
-            ScrollArea::vertical()
-                .auto_shrink([false; 2])
-                .max_height(ui.available_height() - 25.)
-                .show(ui, |ui| {
-                    ui.label(&self.text);
-                });
-            ui.with_layout(egui::Layout::right_to_left(egui::Align::RIGHT), |ui| {
-                if ui.button("Clear").clicked() {
-                    self.text.clear();
+                ui.heading("Rustier");
+                if ui.button("+").clicked() {
+                    self.num_tiers += 1;
+                    self.tiers.push(self.num_tiers.to_string().to_owned());
                 }
             });
-            if ctx.input(|i| i.key_pressed(Key::Space)) {
-                self.text.push_str("\nPressed");
-            }
-            if ctx.input(|i| i.key_down(Key::Space)) {
-                self.text.push_str("\nHeld");
-                ui.ctx().request_repaint(); // make sure we note the holding.
-            }
-            if ctx.input(|i| i.key_released(Key::Space)) {
-                self.text.push_str("\nReleased");
-            }
+
+            ScrollArea::vertical()
+                .auto_shrink([false; 2])
+                .max_height(ui.available_height())
+                .show(ui, |ui| {
+                    for row in 0..self.num_tiers {
+                        ui.add_sized(
+                            [tier_size, tier_size],
+                            Label::new(self.tiers.get(row as usize).unwrap()),
+                        );
+                    }
+                });
         });
     }
+}
+
+fn load_image_from_path(path: &std::path::Path) -> Result<egui::ColorImage, image::ImageError> {
+    let image = image::io::Reader::open(path)?.decode()?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+    Ok(egui::ColorImage::from_rgba_unmultiplied(
+        size,
+        pixels.as_slice(),
+    ))
 }
