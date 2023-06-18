c1fb0a5ecbdbca3ee37dcb758264f5f4bb8ef971
Jona Ohme <ohmejona@gmail.com>
2023-06-19 01:57:24 +0200

    track mouse pos

--- distancer/src/main.rs
+++ distancer/src/main.rs
@@ -12,34 +12,24 @@ fn main() -> Result<(), eframe::Error> {
     )
 }
 
-struct MyApp {
-    name: String,
-    age: u32,
-}
+struct MyApp {}
 
 impl Default for MyApp {
     fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
+        Self {}
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui Application");
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Click each year").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-        });
+        let resp = egui::CentralPanel::default()
+            .show(ctx, |ui| {
+                ui.heading("JO");
+            })
+            .response;
+
+        if let Some(pos) = resp.hover_pos() {
+            eprintln!("{} {}", pos.x, pos.y);
+        }
     }
 }
